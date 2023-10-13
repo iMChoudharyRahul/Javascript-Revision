57e523c71a4f6169a6ec516efe0dc63e759cd659
Choudhary8055 <rahulchoudhary200599@gmail.com>
2023-10-13 18:09:43 +0530

    Closure and Currying function

---
+++ Function/Closure.js
@@ -0,0 +1,42 @@
+/**
+ *  A Closure is js feature thats allow inner function to access outer function scope.
+ *  function along with its lexical scope is called a closure
+ *  task: In personal finance applications, you often want to track income, expenses, and calculate account balances.
+ *        Closures can help in creating encapsulated and secure financial trackers.
+ *   
+ */
+function createAccount(initialBalance) {
+    let balance = initialBalance;
+    console.log("Starting Balance:", balance);
+  
+    // Return an object with methods to interact with the account
+    return {
+      deposit: function(amount) {
+        balance += amount;
+        console.log(`Deposited $${amount}`);
+      },
+      withdraw: function(amount) {
+        if (amount <= balance) {
+          balance -= amount;
+          console.log(`Withdrawn $${amount}`);
+        } else {
+          console.log("Insufficient balance.");
+        }
+      },
+      getBalance: function() {
+        return balance;
+      }
+    };
+  }
+  
+  const account1 = createAccount(1000);
+  const account2 = createAccount(500);
+  
+  account1.deposit(200);
+  account1.withdraw(300);
+  console.log(`Account 1 balance: $${account1.getBalance()}`);
+  
+  account2.deposit(300);
+  account2.withdraw(700);
+  console.log(`Account 2 balance: $${account2.getBalance()}`);
+  
---
+++ Function/Currying.js
@@ -0,0 +1,47 @@
+/**
+ * currying is advanced function writing process that takes multiple argument is transformed into a series of function, each with single argument.
+ * currying helps us to avoid passing the same variable multiple time.
+ */
+
+function curriedAdd(a) {
+    return function (b) {
+      return function (c) {
+        return a + b + c;
+      };
+    };
+  }
+
+  const result = curriedAdd(1)(2)(3); //
+  
+
+ /**
+  * Routing is a fundamental part of building web applications, and currying can be used to create a flexible and modular routing system. 
+  */
+  // Curried routing function
+const route = (path) => (handler) => {
+    console.log(`Route defined for path: ${path}`);
+    // Here, you would typically set up a mapping between the path and the handler function
+    // For simplicity, we'll just log the route definition.
+  };
+  
+  // Define routes using currying
+  const createRoutes = (router) => {
+    return {
+      home: router('/'),
+      about: router('/about'),
+      products: router('/products'),
+      contact: router('/contact')
+    };
+  };
+  
+  // Usage
+  const appRouter = route; // Initialize the router
+  
+  const routes = createRoutes(appRouter);
+  
+  // Define route handlers
+  routes.home(() => console.log('Home page'));
+  routes.about(() => console.log('About page'));
+  routes.products(() => console.log('Products page'));
+  routes.contact(() => console.log('Contact page'));
+  
