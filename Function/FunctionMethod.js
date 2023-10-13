013578b4cc75cda6865f090383930062d6bf845b
Choudhary8055 <rahulchoudhary200599@gmail.com>
2023-10-13 11:11:18 +0530

    Function Methods

---
+++ Function/FunctionMethod.js
@@ -0,0 +1,40 @@
+/**
+ * The call, apply, and bind methods are essential features in JavaScript for manipulating the context and arguments of functions.
+ * function have three method
+ * 1. Call: 
+ * 2. Apple
+ * 3. Bind
+ * 
+ * Task: Suppose you're developing an e-commerce platform, and you have a Product object with a method 
+ * for calculating the total price based on quantity and applying a discount. 
+ * You might have different use cases where you want to calculate the total price for a product with different parameters.
+ * Solution:  we have multiple product object and they have diff-diff price, quantity and discount 
+ *      for every product object we don't need write again and agin same function so we use call, apply and bind method 
+ */
+const Product = {
+   name: "lenova laptop",
+   price: 40000,
+   calculateTotal: function(quantity, discount){
+      return (this.price * quantity) * (1 - discount);
+   }
+};
+// Use 'call' to calculate the total price with custom context and arguments
+const customProduct = {
+  name: "IPhone 14 plus",
+  price: 60000
+}
+
+//we don't need to write again and calculateTotal using Call  
+const TotalProductPrice = Product.calculateTotal(4, 0.2);
+console.log(`Total for ${Product.name}: $${TotalProductPrice}`);
+const TotalPrice = Product.calculateTotal.call(customProduct, 2, 0.1);
+console.log(`Total for ${customProduct.name}: $${TotalPrice}`);
+
+//Using Apply Method
+const customPrice = Product.calculateTotal.apply(customProduct, [2, 0.1]);
+console.log(`Total for ${customProduct.name}: $${customPrice} using apply method`);
+
+//Using Bind Method
+const useBind = Product.calculateTotal.bind(customProduct, 2, 0.1);
+let total = useBind();
+console.log(`Total for ${customProduct.name}: $${total} using bind method`);
